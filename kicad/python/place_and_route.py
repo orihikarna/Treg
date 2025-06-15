3937277d220f37565570d9f96c727556f3ba3e40
orihikarna <orihikarna@gmail.com>
2025-06-15 19:44:39 +0900

    wiring pwr

--- kicad/python/place_and_route.py
+++ kicad/python/place_and_route.py
@@ -37,7 +37,8 @@ pcb = pcbnew.GetBoard()
 #         print(n)
 
 GND = pcb.FindNet("GND")
-VCC = pcb.FindNet("3V3")
+V1_9 = pcb.FindNet("1V9")
+V3_3 = pcb.FindNet("3V3")
 
 board_width = 34
 board_height = 44  # 2.54 * 7
@@ -131,14 +132,13 @@ def wire_mod():
     ]
 
     via_gnd_C1 = kad.add_via_relative("C1", "2", (0, +1.4), via_size_pwr)
-    via_3v3_C4 = kad.add_via_relative("C4", "1", (0, -1.3), via_size_pwr)
-    via_gnd_C4 = kad.add_via_relative("C4", "2", (0, -1.3), via_size_pwr)
+    via_3v3_C4 = kad.add_via_relative("C4", "1", (0.4, -1.6), via_size_pwr)
     via_1V9_C6 = kad.add_via_relative("C6", "1", (0, -1.3), via_size_pwr)
     via_3v3_C9 = kad.add_via_relative("C9", "1", (-1.4, 0), via_size_pwr)
     via_1V9_C8 = kad.add_via_relative("C8", "1", (-1.4, 0), via_size_pwr)
     via_1V9_C13 = kad.add_via_relative("C13", "1", (0, -1.4), via_size_pwr)
     via_gnd_CS = kad.add_via_relative(pmw, "13", (-1.9, 0), via_size_pwr)  # NCS = GND
-    via_gnd_pmw = kad.add_via(vec2.add(kad.get_mod_pos(pmw), (5.4, +9.6)), GND, via_size_pwr)
+    # via_gnd_pmw = kad.add_via(vec2.add(kad.get_mod_pos(pmw), (5.4, +9.6)), GND, via_size_pwr)
 
     kad.wire_mod_pads(
         [
@@ -151,10 +151,9 @@ def wire_mod():
             ("C6", "1", None, via_1V9_C6, w_dat, (Dird, 0, 90, 0)),  # 1V9
             (pmw, "5", "C3", "1", w_dat, (Dird, +45, 0, r_dat)),  # 3V4
             ("C3", "1", "C4", "1", w_dat, (Strt)),
-            (pmw, "7", xiao, "7", w_dat, (Dird, [(0, 0.18), -128], 90, r_dat), "B.Cu"),  # nRESET
-            ("C4", "1", None, via_3v3_C4, w_pwr, (Strt)),  # 3V3
-            (pmw, "8", None, via_gnd_C4, w_pwr, (Dird, 0, 90, r_pwr), "F.Cu"),  # GND
-            (pmw, via_gnd_C1, None, via_gnd_C4, w_pwr, (Strt)),  # vertical GND
+            (pmw, "7", "J7", "7", w_dat, (Dird, [(-45, 1.2), 180], [(-45, 1.6), 90], r_dat), "B.Cu"),  # nRESET
+            ("C4", "1", None, via_3v3_C4, w_pwr, (Dird, 90, 0)),  # 3V3
+            (pmw, "8", None, via_gnd_C1, w_pwr, (Dird, 0, 90, 1.6), "F.Cu"),  # GND
             # right
             ("C8", "1", None, via_1V9_C8, w_dat, (Strt)),
             (pmw, "13", None, via_gnd_CS, w_dat, (Strt)),  # NCS
@@ -186,6 +185,7 @@ def wire_mod():
             ("C14", "1", "C13", "1", w_pwr, (Dird, [(180, 1.0), 135], [(180, 1.0), 90], 0.4)),
             ### LDO <--> pmw
             ("C8", via_1V9_C8, "C13", via_1V9_C13, w_pwr, (Dird, 0, 90, r_pwr)),
+            # ("C8", via_1V9_C6, "C8", via_1V9_C8, w_pwr, (Dird, 0, 90, r_pwr)),
             ### LD0 <--> xiao
             (xiao, "13", "C12", "2", w_pwr, (Dird, 0, 90, r_pwr), "F.Cu"),  # GND
             (xiao, "14", "C12", via_ldo_vbus[0], w_pwr, (Dird, 0, 90, r_pwr), "B.Cu"),  # VBUS
@@ -209,8 +209,8 @@ def wire_mod():
                 (f"R{11+2*n}", "1", None, via_sw_3v3[n], w_dat, (Dird, 90, 0, 0)),
             ]
         )
-    for n in range(4):
-        kad.wire_mod_pads([(f"J{n+1}", "2", f"J{n+2}", "2", w_pwr, (Dird, [(0, 2), 90], 0, r_pwr), "B.Cu")])
+    # for n in range(4):
+    #     kad.wire_mod_pads([(f"J{n+1}", "2", f"J{n+2}", "2", w_pwr, (Dird, [(0, 2), 90], 0, r_pwr), "B.Cu")])
 
     # SW --> xiao
     r_sw = 1.2
@@ -226,6 +226,8 @@ def wire_mod():
 
     # xiao --> pmw pwr
     via_pmw_top = kad.add_via(vec2.add(kad.get_mod_pos(pmw), (0, +10.4)), GND, via_size_pwr)
+    via_pmw_1V9 = kad.add_via(vec2.add(kad.get_mod_pos(pmw), (0, +10.0)), V1_9, via_size_pwr)
+    via_pmw_3V3 = kad.add_via(vec2.add(kad.get_mod_pos(pmw), (0, +11.0)), V3_3, via_size_pwr)
     kad.wire_mod_pads(
         [
             (pmw, via_3v3_C4, None, via_pmw_top, w_pwr, (Dird, 90, 0, r_pwr), "In1.Cu"),  # 3V3
